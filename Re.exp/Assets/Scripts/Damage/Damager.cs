5efdbd2f2aa7f65fe9db568428cc50f7a0ba379e
aleksander431 <alex23_2001@mail.ru>
2022-03-15 12:37:47 +0300

    New Player Obj

--- Re.exp/Assets/Scripts/Damage/Damager.cs
+++ Re.exp/Assets/Scripts/Damage/Damager.cs
@@ -9,9 +9,17 @@ public class Damager : MonoBehaviour
    public bool type;
    private void OnTriggerEnter(Collider other)
    {
+      Debug.Log(1);
       damagable = other.gameObject.GetComponent<Damagable>();
       if ((damagable != null) && (damagable.type != type)){
          damagable.DamageDeal(damage);
       }
    }
+   private void OnCollisionEnter(Collision other){
+      damagable = other.gameObject.GetComponent<Damagable>();
+      if ((damagable != null) && (damagable.type != type))
+      damagable.DamageDeal(damage);
+   }
+
+
 }
